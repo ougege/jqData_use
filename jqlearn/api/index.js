3b059ecd7b940df8e18326bc010669c4657500f0
Unknown <unknown@example.com>
2020-05-29 23:37:10 +0800

    futertick收费，改用tqsdk

--- jqlearn/api/index.js
+++
@@ -1,8 +0,0 @@
-// api文档：https://dataapi.joinquant.com/docs
-const api_login = require('./api_login')
-const api_query = require('./api_query')
-const api = {}
-// 追加属性
-Object.assign(api, api_login)
-Object.assign(api, api_query)
-module.exports = api
